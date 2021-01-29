8fdee6a9ad10e1ba3ade4a6d2f3286391e49c3e5
leocalmeida <leonardocaetanoalmeida@gmail.com>
2021-01-29 17:52:15 -0300

    inicio forca

--- Cap05/Lab03/forca_v1.py
+++ Cap05/Lab03/forca_v1.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Hangman Game (Jogo da Forca) 
 # Programação Orientada a Objetos
 
@@ -71,31 +72,35 @@ class Hangman:
 
 	# Método Construtor
 	def __init__(self, word):
-		
+		self.word = word
 		
 	# Método para adivinhar a letra
 	def guess(self, letter):
-		
+		if letter in self.word:
+			print("tem")
+		else:
+			print("n tem")
+
 		
 	# Método para verificar se o jogo terminou
-	def hangman_over(self):
+	# def hangman_over(self):
 		
 		
 	# Método para verificar se o jogador venceu
-	def hangman_won(self):
+	# def hangman_won(self):
 		
 
 	# Método para não mostrar a letra no board
-	def hide_word(self):
+	# def hide_word(self):
 		
 		
 	# Método para checar o status do game e imprimir o board na tela
-	def print_game_status(self):
+	# def print_game_status(self):
 		
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
 def rand_word():
-        with open("palavras.txt", "rt") as f:
+        with open("arquivos/palavras.txt", "rt") as f:
                 bank = f.readlines()
         return bank[random.randint(0,len(bank))].strip()
 
@@ -106,20 +111,22 @@ def main():
 	# Objeto
 	game = Hangman(rand_word())
 
-	# Enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
-	
+	# Enquanto o jogo não tiver terminado, 
+	# print do status, 
+	# solicita uma letra e faz a leitura do caracter
+	game.guess(input('Digite uma letra: '))
 
 	# Verifica o status do jogo
-	game.print_game_status()	
+	# game.print_game_status()	
 
 	# De acordo com o status, imprime mensagem na tela para o usuário
-	if game.hangman_won():
-		print ('\nParabéns! Você venceu!!')
-	else:
-		print ('\nGame over! Você perdeu.')
-		print ('A palavra era ' + game.word)
+	# if game.hangman_won():
+	# 	print ('\nParabéns! Você venceu!!')
+	# else:
+	# 	print ('\nGame over! Você perdeu.')
+	# 	print ('A palavra era ' + game.word)
 		
-	print ('\nFoi bom jogar com você! Agora vá estudar!\n')
+	# print ('\nFoi bom jogar com você! Agora vá estudar!\n')
 
 # Executa o programa		
 if __name__ == "__main__":
